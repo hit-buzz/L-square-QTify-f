62796399b4d9adf2468277bdc8c09188a2b65dfc
apurv <apurv@elevate.com>
2023-10-08 12:25:26 +0530

    search bar

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import styles from './Search.module.css';
+import {ReactComponent as SearchIcon} from '../../assets/Search icon.svg'
 
 const Search = () => {
   return (
-    <form>
-       <input />
-       <button>
-        
+    <form className={styles.wrapper}>
+       <input className={styles.search} />
+       <button className={styles.searchButton}>
+        <SearchIcon/>
         </button> 
     </form>
   )
