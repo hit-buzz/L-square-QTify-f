69d03b0b9c976c10f40b7733824cbfe36c7c4d2c
apurv <apurv@elevate.com>
2023-10-08 15:06:47 +0530

    semi-final

--- qtify/src/components/HeroSection/HeroSection.jsx
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import HeroImage from '../../assets/vibrating-headphone 1.png';
+import styles from './HeroSection.module.css'
 
 const HeroSection = () => {
   return (
-    <div className="hero">
-      <img src={HeroImage} alt="Logo" />
-      <h1>100 Thousand Songs, ad-free</h1>
-      <p>Over thousands of podcast episodes</p>
+    <div className={styles.hero}>
+      <div className={styles.heroContent}>
+        < div classname={styles.heroContent1}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands of podcast episodes</h1>
+      </div>
+      <div><img src={HeroImage} alt="Logo"  height ="212px" width= "212px"/></div>
+      </div>
     </div>
   );
 };
